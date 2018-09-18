d12580b106e09fcff61928f608623fe56029cb1c
yutingcxiang <yutingcxiang@gmail.com>
2018-09-17 23:16:39 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,10 @@
 class ArtistsController < ApplicationController
   def index
-    @artists = Artist.all
+    if Preference.first && Preference.first.artist_sort_order
+      @artists = Artist.order(name: Preference.first.artist_sort_order)
+    else
+      @artists = Artist.all
+    end
   end
 
   def show
@@ -8,7 +12,11 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    @artist = Artist.new
+    if Preference.first.allow_create_artists
+      @artist = Artist.new
+    else
+      redirect_to artists_path
+    end
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   resources :artists do
     resources :songs, only: [:index, :show]
   end
+  
   resources :songs
 
+  namespace :admin do
+    resources :preferences, only: [:index]
+  end
 end
